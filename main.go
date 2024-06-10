fb82e69027a1cac05135e466c174ca03adf698f9
uni51 <unieye51@gmail.com>
2024-06-10 23:26:24 +0900

    1-2 ハンドラを増やそう

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/yourname/reponame/handlers"
+)
+
+func main() {
+
+	// 定義した helloHandler を使うように登録
+	http.HandleFunc("/hello", handlers.HelloHandler)
+	http.HandleFunc("/article", handlers.PostArticleHandler)
+	http.HandleFunc("/article/list", handlers.ArticleListHandler)
+	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	http.HandleFunc("/comment", handlers.PostCommentHandler)
+
+	// サーバー起動時のログを出力
+	log.Println("server start at port 8080")
+
+	// ListenAndServe関数にて、サーバーを起動
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}

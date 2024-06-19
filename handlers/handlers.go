5f5c2de4b0b3ced328104024303529740c5108dc
uni51 <unieye51@gmail.com>
2024-06-19 17:48:48 +0900

    1-6. クエリパラメータを取得する

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 // /hello のハンドラ
@@ -17,11 +20,37 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article List\n")
+	queryMap := req.URL.Query()
+
+	var page int
+	// もしmap型の変数queryMapが文字列"page"をキーに持っているのであれば、pにはpage キーに対応する値 queryMap["page"] が、ok には true が格納される
+	if p, ok := queryMap["page"]; ok && len(p) > 0 {
+		// パラメータ page に対応する 1 つ目の値を採用し、それを数値に変換する
+		var err error
+		page, err = strconv.Atoi(p[0])
+
+		// 数値に変換できない値だった場合は 400 番エラーを返す
+		if err != nil {
+			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+			return
+		}
+		// パラメータ page が存在しなかった場合
+	} else {
+		// パラメータ page=1 がついていたときと同じ処理をしたい
+		page = 1
+	}
+
+	resString := fmt.Sprintf("Article List (page: %d)\n", page)
+	io.WriteString(w, resString)
 }
 
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	articleID := 1
+	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
+	if err != nil {
+		// クエリパラメータが数字でない場合は、400 Bad Requestを返す
+		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+		return
+	}
 	resString := fmt.Sprintf("Article No.%d\n", articleID)
 	io.WriteString(w, resString)
 }
